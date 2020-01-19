d65f58f0a6fed5a91f18f39474aa211203989a9f
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-01-19 21:25:39 +0100

    Adding Query Params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { map, catchError } from 'rxjs/operators';
 
@@ -34,10 +34,13 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
     return this.http
       .get<{ [key: string]: Post }>('https://ng-guide-c2bed.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams
         }
       )
       .pipe(
