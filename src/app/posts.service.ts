3348f068e1ec59a73fffbc885c6241dc4db1dd22
Julio <juliorodriguezramirez@gmail.com>
2020-01-19 20:23:21 +0100

    Test setting headers

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { map, catchError } from 'rxjs/operators';
 
@@ -35,7 +35,11 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>('https://ng-guide-c2bed.firebaseio.com/posts.json')
+      .get<{ [key: string]: Post }>('https://ng-guide-c2bed.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+        }
+      )
       .pipe(
         map(respData => {
           const postsArray: Post[] = [];
